9dc187968d3b88b3a4f9566b800fd30bc2e8ee4e
Miaoli Jiang <76165158+aoaow@users.noreply.github.com>
2025-04-23 00:13:19 -0400

    1

--- preprocessing.py
+++ preprocessing.py
@@ -3,6 +3,9 @@ import pandas as pd
 import numpy as np
 
 def load_data():
+    """
+    Directly access the rData as dataframe
+    """
     data = pyreadr.read_r('resume.rda')['resume'] # pathway
     df = pd.DataFrame(data)
 
